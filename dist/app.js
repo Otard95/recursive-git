db1aa740c05eef3bca45ab78daaa6bb02e24c01f
Stian Myklebostad <stian.myklebostad@getmail.no>
2019-01-25 15:35:15 +0100

    Main window now ruunning isolated
    still security warning will look into it

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,13 @@ const electron_1 = require("electron");
 const path_1 = __importDefault(require("path"));
 let win;
 function createWindow() {
-    win = new electron_1.BrowserWindow({ width: 1000, height: 800 });
+    win = new electron_1.BrowserWindow({
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+        },
+        width: 1000,
+    });
     win.loadFile(path_1.default.resolve('./dist/index.html'));
 }
 electron_1.app.on('ready', createWindow);
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import path from 'path';
 let win: BrowserWindow;
 
 function createWindow () {
-  win = new BrowserWindow({width: 1000, height: 800});
+  win = new BrowserWindow({
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+    width: 1000,
+  });
   
   win.loadFile(path.resolve('./dist/index.html'));
 }
